1c44f4974a222d7bf128cb40e8ce30fcf2e0babb
matchashiba <recr38@gmail.com>
2020-08-03 21:28:49 +0000

    Added Bootstrap link and script to index.html Created initial Carousel component imported Carousel to App

---
+++ client/components/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Carousel(props) {
+  return (
+    <React.Fragment>
+      <div> I am a carousel </div>
+    </React.Fragment>
+  );
+}
+
+export default Carousel;
